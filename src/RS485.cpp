6d25d5ff1e6fb5f964bfc99237811aa97f1aaf8b
gastonfeng <gastonfeng@gmail.com>
2018-11-23 10:47:24 +0800

    submodule

--- src/RS485.cpp
+++ src/RS485.cpp
@@ -18,13 +18,35 @@
 */
 
 #include "RS485.h"
+#include <libmaple/usart.h>
 
-RS485Class::RS485Class(HardwareSerial& hwSerial, int txPin, int dePin, int rePin) :
-  _serial(&hwSerial),
-  _txPin(txPin),
-  _dePin(dePin),
-  _rePin(rePin),
-  _transmisionBegun(false)
+void usart_disable_rx(usart_dev *dev) {
+    usart_reg_map *regs = dev->regs;
+    // while (!rb_is_empty(dev->wb))
+    //     ; // wait for TX completed
+    // /* TC bit must be high before disabling the USART */
+    // while ((regs->CR1 & USART_CR1_UE) && !(regs->SR & USART_SR_TC))
+    //     ;
+
+    regs->CR1 &= ~USART_CR1_RE; // don't change the word length etc, and 'or' in the patten not overwrite |USART_CR1_M_8N1);
+    usart_reset_rx(dev);
+}
+
+void usart_enable_rx(usart_dev *dev) {
+    usart_reg_map *regs = dev->regs;
+    while ((regs->CR1 & USART_CR1_UE) && !(regs->SR & USART_SR_TC));
+    regs->CR1 |= (USART_CR1_TE | USART_CR1_RE |
+                  USART_CR1_RXNEIE); // don't change the word length etc, and 'or' in the patten not overwrite |USART_CR1_M_8N1);
+    regs->CR1 |= USART_CR1_UE;
+}
+
+RS485Class::RS485Class(usart_dev *usart_device, int txPin, uint8 rx_pin, int dePin, int rePin) : HardwareSerial(
+        usart_device, txPin, rx_pin),
+                                                                                                 _txPin(txPin),
+                                                                                                 _dePin(dePin),
+                                                                                                 _rePin(rePin),
+                                                                                                 _transmisionBegun(
+                                                                                                         false)
 {
 }
 
@@ -38,31 +60,31 @@ void RS485Class::begin(unsigned long baudrate, uint16_t config)
   _baudrate = baudrate;
   _config = config;
 
-  if (_dePin > -1) {
+    if (_dePin > -1) {
     pinMode(_dePin, OUTPUT);
     digitalWrite(_dePin, LOW);
   }
 
-  if (_rePin > -1) {
+    if (_rePin > -1) {
     pinMode(_rePin, OUTPUT);
     digitalWrite(_rePin, HIGH);
   }
 
   _transmisionBegun = false;
 
-  _serial->begin(baudrate, config);
+    this->begin(baudrate, config);
 }
 
 void RS485Class::end()
 {
-  _serial->end();
+    this->end();
 
-  if (_rePin > -1) {
+    if (_rePin > -1) {
     digitalWrite(_rePin, LOW);
     pinMode(_dePin, INPUT);
   }
-  
-  if (_dePin > -1) {
+
+    if (_dePin > -1) {
     digitalWrite(_dePin, LOW);
     pinMode(_rePin, INPUT);
   }
@@ -70,32 +92,32 @@ void RS485Class::end()
 
 int RS485Class::available()
 {
-  return _serial->available();
+    return this->available();
 }
 
 int RS485Class::peek()
 {
-  return _serial->peek();
+    return this->peek();
 }
 
 int RS485Class::read(void)
 {
-  return _serial->read();
+    return this->read();
 }
 
 void RS485Class::flush()
 {
-  return _serial->flush();
+    return this->flush();
 }
 
 size_t RS485Class::write(uint8_t b)
 {
-  if (!_transmisionBegun) {
+    if (!_transmisionBegun) {
     setWriteError();
     return 0;
   }
 
-  return _serial->write(b);
+    return this->write(b);
 }
 
 RS485Class::operator bool()
@@ -105,7 +127,7 @@ RS485Class::operator bool()
 
 void RS485Class::beginTransmission()
 {
-  if (_dePin > -1) {
+    if (_dePin > -1) {
     digitalWrite(_dePin, HIGH);
     delayMicroseconds(50);
   }
@@ -115,9 +137,9 @@ void RS485Class::beginTransmission()
 
 void RS485Class::endTransmission()
 {
-  _serial->flush();
+    this->flush();
 
-  if (_dePin > -1) {
+    if (_dePin > -1) {
     delayMicroseconds(50);
     digitalWrite(_dePin, LOW);
   }
@@ -127,36 +149,38 @@ void RS485Class::endTransmission()
 
 void RS485Class::receive()
 {
-  if (_rePin > -1) {
+    if (_rePin > -1) {
     digitalWrite(_rePin, LOW);
   }
+    usart_enable_rx(this->c_dev());
 }
 
 void RS485Class::noReceive()
 {
-  if (_rePin > -1) {
+    if (_rePin > -1) {
     digitalWrite(_rePin, HIGH);
   }
+    usart_disable_rx(this->c_dev());
 }
 
 void RS485Class::sendBreak(unsigned int duration)
 {
-  _serial->flush();
-  _serial->end();
+    this->flush();
+    this->end();
   pinMode(_txPin, OUTPUT);
   digitalWrite(_txPin, LOW);
   delay(duration);
-  _serial->begin(_baudrate, _config);
+    this->begin(_baudrate, _config);
 }
 
 void RS485Class::sendBreakMicroseconds(unsigned int duration)
 {
-  _serial->flush();
-  _serial->end();
+    this->flush();
+    this->end();
   pinMode(_txPin, OUTPUT);
   digitalWrite(_txPin, LOW);
   delayMicroseconds(duration);
-  _serial->begin(_baudrate, _config);
+    this->begin(_baudrate, _config);
 }
 
 void RS485Class::setPins(int txPin, int dePin, int rePin)
@@ -166,4 +190,4 @@ void RS485Class::setPins(int txPin, int dePin, int rePin)
   _rePin = rePin;
 }
 
-RS485Class RS485(SERIAL_PORT_HARDWARE, RS485_DEFAULT_TX_PIN, RS845_DEFAULT_DE_PIN, RS845_DEFAULT_RE_PIN);
+//RS485Class RS485(SERIAL_PORT_HARDWARE, RS485_DEFAULT_TX_PIN, RS845_DEFAULT_DE_PIN, RS845_DEFAULT_RE_PIN);
--- src/RS485.h
+++ src/RS485.h
@@ -21,48 +21,68 @@
 #define _RS485_H_INCLUDED
 
 #include <Arduino.h>
-
+#include <libmaple/usart.h>
 #ifdef PIN_SERIAL1_TX
 #define RS485_DEFAULT_TX_PIN PIN_SERIAL1_TX
 #else
-#define RS485_DEFAULT_TX_PIN 1 
+#define RS485_DEFAULT_TX_PIN 1
 #endif
 
 #ifdef __AVR__
 #define RS845_DEFAULT_DE_PIN 2
 #define RS845_DEFAULT_RE_PIN -1
 #else
-#define RS845_DEFAULT_DE_PIN A6
-#define RS845_DEFAULT_RE_PIN A5
+#define RS845_DEFAULT_DE_PIN 0
+#define RS845_DEFAULT_RE_PIN 0
 #endif
+#define DEFINE_RS485(name, n, depin, repin)           \
+    RS485Class name(USART##n,                \
+               BOARD_USART##n##_TX_PIN, \
+               BOARD_USART##n##_RX_PIN,depin,repin)
+#define DEFINE_HWSERIAL_UART_485(name, n, depin, repin) \
+    RS485Class name(UART##n,                   \
+               BOARD_USART##n##_TX_PIN,   \
+               BOARD_USART##n##_RX_PIN,depin,repin)
 
-class RS485Class : public Stream {
-  public:
-    RS485Class(HardwareSerial& hwSerial, int txPin, int dePin, int rePin);
+class RS485Class : public HardwareSerial {
+public:
+    RS485Class(usart_dev *usart_device, int txPin, uint8 rx_pin, int dePin, int rePin);
 
     virtual void begin(unsigned long baudrate);
+
     virtual void begin(unsigned long baudrate, uint16_t config);
+
     virtual void end();
+
     virtual int available();
+
     virtual int peek();
+
     virtual int read(void);
+
     virtual void flush();
+
     virtual size_t write(uint8_t b);
+
     using Print::write; // pull in write(str) and write(buf, size) from Print
     virtual operator bool();
 
     void beginTransmission();
+
     void endTransmission();
+
     void receive();
+
     void noReceive();
 
     void sendBreak(unsigned int duration);
+
     void sendBreakMicroseconds(unsigned int duration);
 
     void setPins(int txPin, int dePin, int rePin);
 
-  private:
-    HardwareSerial* _serial;
+private:
+    //HardwareSerial* _serial;
     int _txPin;
     int _dePin;
     int _rePin;
